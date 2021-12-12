21fa8dc3b3057698d04dd868fc981d48e19bfa23
d.sarkulin <sarkulindamir@gmail.com>
2021-12-13 03:04:18 +0500

    Added API version and routes.

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\DocumentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('documents', DocumentController::class);
+
+Route::post('/documents/{id}/publish', [DocumentController::class, 'publish'])->name('documents.publish');
